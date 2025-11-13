f08647f0264ce51c9111b7fd4d70f85894f60cbc
nsvk13 <nsvkdev@gmail.com>
2025-11-13 05:46:54 +0300

    feat: importing metrics for collections

--- exporter.py
+++ exporter.py
@@ -1,5 +1,10 @@
 from prometheus_client import start_http_server
 from collectors.cpu import collect_cpu
+from collectors.memory import collect_memory
+from collectors.disk import collect_disks
+from collectors.network import collect_network
+from collectors.uptime import collect_uptime
+from collectors.gpu import collect_gpu
 
 import time
 
@@ -8,4 +13,9 @@ if __name__ == "__main__":
 
     while True:
         collect_cpu()
+        collect_memory()
+        collect_disks()
+        collect_network()
+        collect_uptime()
+        collect_gpu()
         time.sleep(2)
